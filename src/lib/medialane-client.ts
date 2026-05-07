9f812519b67ea1d611cd34bc0cf7e86039de4b0e
salvador camino <salvadorcamino@gmail.com>
2026-05-07 17:09:41 -0300

    Align SDK and shared UI versions

--- src/lib/medialane-client.ts
+++ src/lib/medialane-client.ts
@@ -1,5 +1,13 @@
 import { MedialaneClient } from "@medialane/sdk";
-import { MEDIALANE_BACKEND_URL, MEDIALANE_API_KEY, STARKNET_RPC_URL, MARKETPLACE_721_CONTRACT } from "./constants";
+import {
+  COLLECTION_1155_CONTRACT,
+  COLLECTION_721_CONTRACT,
+  MARKETPLACE_1155_CONTRACT,
+  MARKETPLACE_721_CONTRACT,
+  MEDIALANE_API_KEY,
+  MEDIALANE_BACKEND_URL,
+  STARKNET_RPC_URL,
+} from "./constants";
 
 let _client: MedialaneClient | null = null;
 
@@ -10,6 +18,9 @@ export function getMedialaneClient(): MedialaneClient {
       apiKey: MEDIALANE_API_KEY || undefined,
       rpcUrl: STARKNET_RPC_URL || undefined,
       marketplaceContract: MARKETPLACE_721_CONTRACT,
+      marketplace1155Contract: MARKETPLACE_1155_CONTRACT,
+      collectionContract: COLLECTION_721_CONTRACT,
+      collection1155Contract: COLLECTION_1155_CONTRACT,
       network: "mainnet",
     });
   }
